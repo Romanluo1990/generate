feb9702a79889e191cd3f29b5778c8ca4a34d7df
roman.luo <roman.luo@vipshop.com>
2018-07-09 15:03:08 +0800

    初始化基本框架代码

--- dao/src/main/java/com/vip/xpf/dao/BaseDao.java
+++ dao/src/main/java/com/vip/xpf/dao/BaseDao.java
@@ -5,48 +5,48 @@ import com.vip.xpf.model.Identity;
 
 public interface BaseDao<E extends Identity> extends Dao {
 
-    /**
-     * 保存
-     * @param e
-     * @return
-     */
-    boolean save(E e);
+	/**
+	 * 保存
+	 * @param e
+	 * @return
+	 */
+	boolean save(E e);
 
-    /**
-     * 按id删除
-     * @param id
-     * @return
-     */
-    boolean deleteById(long id);
+	/**
+	 * 按id删除
+	 * @param id
+	 * @return
+	 */
+	boolean deleteById(long id);
 
-    /**
-     * 按id更新全字段
-     * @param e
-     * @return
-     */
-    boolean updateById(E e);
+	/**
+	 * 按id更新全字段
+	 * @param e
+	 * @return
+	 */
+	boolean updateById(E e);
 
-    /**
-     * 按id更新非空字段
-     * @param e
-     * @return
-     */
-    boolean updateByIdSelective(E e);
+	/**
+	 * 按id更新非空字段
+	 * @param e
+	 * @return
+	 */
+	boolean updateByIdSelective(E e);
 
-    /**
-     * 保存或更新，按id判断
-     * @param e
-     * @return
-     */
-    boolean saveOrUpdate(E e);
+	/**
+	 * 保存或更新，按id判断
+	 * @param e
+	 * @return
+	 */
+	boolean saveOrUpdate(E e);
 
-    /**
-     * 按id查数据
-     * @param id
-     * @return
-     */
-    E getById(long id);
+	/**
+	 * 按id查数据
+	 * @param id
+	 * @return
+	 */
+	E getById(long id);
 
-    long count();
+	long count();
 
 }
